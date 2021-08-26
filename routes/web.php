34f57900b2f7df1511adc340294b3e08c49d00a8
Khairul Alam <khairul@Admins-iMac.local>
2021-08-25 22:05:59 -0400

    Added new middleware for admin authorization

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\NewsletterController;
-
+use App\Http\Middleware\MustBeAdministrator;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 Route::post('newsletter', NewsletterController::class);
 
-Route::get('admin/posts/create', [PostController::class, 'create']);
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
 
 Route::get('register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'create'])->middleware('guest');
