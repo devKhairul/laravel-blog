42ea48fef664ceadd025b2a36021a4b139fa026c
Khairul Alam <khairul@Admins-iMac.local>
2021-09-19 19:53:17 -0400

    Updated logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,7 +25,7 @@
         <nav class="md:flex md:justify-between md:items-center">
             <div>
                 <a href="/">
-                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                    <strong class="text-2xl"><span class="text-sm text-green-500">hello</span>Laravel</strong>
                 </a>
             </div>
 
